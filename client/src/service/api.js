59bd01f803d9987eca2236db6fa4ca417a293513
Lokesh Wani <95117860+Lpwani@users.noreply.github.com>
2024-03-14 20:33:05 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:8000';
+const URL = 'https://flipkartclonewebapp-api.vercel.app/';
 
 export const authenticateSignup = async(data) => {
     try {
