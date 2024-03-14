bea3faee3396519da79af1ffd3b97eeca4a44696
Lokesh Wani <95117860+Lpwani@users.noreply.github.com>
2024-03-14 20:42:23 +0530

    Update cartAction.js

--- client/src/redux/actions/cartAction.js
+++ client/src/redux/actions/cartAction.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import * as actionType from "../constants/cartConstant";
 
-const URL = "http://localhost:8000";
+const URL = "https://flipkartclonewebapp-api.vercel.app";
 
 export const addToCart = (id, quantity) => async (dispatch) => {
   try {
