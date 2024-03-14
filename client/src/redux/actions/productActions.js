885f4cdb5ff4d58278fb9ff41571a972613a95e2
Lokesh Wani <95117860+Lpwani@users.noreply.github.com>
2024-03-14 20:54:26 +0530

    Update productActions.js

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 import * as actionTypes from '../constants/productConstants.js';
 
-const URL = 'https://flipkartclonewebapp-api.vercel.app/';
+const URL = 'https://flipkartclonewebapp-api.vercel.app';
 
 export const getProducts = () => async (dispatch) => {
     try {
